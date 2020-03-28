ac60e5020e88a557a000b28f34f82f552a225a6b
anirudh-555 <62657967+anirudh-555@users.noreply.github.com>
2020-03-28 15:05:07 +0530

    Update Ani_proj.c

--- Ani_proj.c
+++ Ani_proj.c
@@ -6,25 +6,26 @@
 #include<unistd.h>
 #define MIN_PID 100
 #define MAX_PID 1000
+//Execute the prrogram ussisng gcc -pthread PROGRAM_NAME
 // Declare our Bitmap
 int bit_map[MAX_PID-MIN_PID];
 int allocate_map() {
-	for(int i = 0; i < MAX_PID-MIN_PID; i++) 
+	for(int i = 0; i < MAX_PID-MIN_PID; i++)
 {
 		bit_map[i] = 0; // Making every pid available by allocating 0 to all bitmaps
-	}
-  printf("Process Bitmap initialized... \n");
+}
+printf("Process Bitmap initialized... \n");
 return 1;
 }
 int allocate_pid() {
 	for(int i = 0; i < MAX_PID-MIN_PID; i++) {
-		if(bit_map[i] == 0) //Making Sure if a pid is available - A pid is available only if its bitmap is 0 
-              { 
+		if(bit_map[i] == 0) //Making Sure if a pid is available - A pid is available only if its bitmap is 0
+              {
 
 			bit_map[i] = 1; // Assigning bitmap to 1 as pid is no longer available
 printf("\033[1;33m");
       printf("New Process Allocated with PID : %d \n",(i+MIN_PID));
-printf("\033[0m");   
+printf("\033[0m");
 int r = 1+rand()%30;
 printf("The thread will take atleast %d seconds to expire\n",r);
 sleep(r);
@@ -38,7 +39,7 @@ void release_pid(int pid) {
       bit_map[pid-MIN_PID] = 0;
 printf("\033[1;31m");
       printf("Process Released with PID : %d \n",pid);
-printf("\033[0m");  
+printf("\033[0m");
 }
 }
 
@@ -50,15 +51,17 @@ void *myThreadFun(void *vargp) {
 
 int main() {
     allocate_map();
+    int no;
+    printf("Enter no of Threads\n");
+    scanf("%d",&no);
     pthread_t tid;
     printf("Let's start creating Thread...\n");
-    for (int i = 0; i < 100; i++)  // Creating 100 Threads for testing
+    for (int i = 0; i < no; i++)  // Creating 100 Threads for testing
     {
     pthread_create(&tid, NULL, myThreadFun, NULL);
 sleep(1);
-}    
+    }
 printf("Successfully Allocated Processes");
     return 0;
 }
-
 
